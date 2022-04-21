45af94a881c9f759633688208847208d9cc2c063
OlexHerman <olexherman@gmail.com>
2022-04-21 21:35:51 +0300

    Added release section

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,16 @@
 $(function(){
 
+  var mixer = mixitup('.release__box-items');
   var $inforate1 = $(".inforate1").rateYo({ "starWidth": "15px", "rating": 4.5, "readOnly": true, "normalFill": "#d1d5dd" });
   var $inforate1 = $(".inforate2").rateYo({ "starWidth": "15px", "rating": 3.2, "readOnly": true, "normalFill": "#d1d5dd" });
   var $inforate1 = $(".inforate3").rateYo({ "starWidth": "15px", "rating": 5.0, "readOnly": true, "normalFill": "#d1d5dd" });
 
-  $(".featured__info-rate").rateYo({
-    starWidth: "15px",
-    readOnly: true,
-    normalFill: "#d1d5dd",
-  });
-  
   $('.featured__content').slick({
     appendArrows: ".featured__slider-btn",
     fade: true
   });
+
+  
 
 });
 
